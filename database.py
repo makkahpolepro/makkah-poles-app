ea4b985d81f2bb85bd692ab722228842e5636e4b
makkahpolepro <shakeralebr25@gmail.com>
2026-08-19 15:15:22 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,14 +2,12 @@ from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# مسار قاعدة بيانات SQLite (سيتم إنشاؤها تلقائياً على السحابة)
 DATABASE_URL = "sqlite:///./makkah_poles.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# تعريف جدول أعمدة الإنارة
 class Pole(Base):
     __tablename__ = "poles"
     id = Column(Integer, primary_key=True, index=True)
@@ -19,6 +17,5 @@ class Pole(Base):
     latitude = Column(Float)
     longitude = Column(Float)
 
-# دالة لإنشاء الجدول عند التشغيل
 def init_db():
     Base.metadata.create_all(bind=engine)
